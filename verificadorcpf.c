b4c761500e7aff63fece6678b0a56cd1f0c7757c
Luccas Miguel <110349383+LuccasMiguel1@users.noreply.github.com>
2024-08-31 09:25:48 -0300

    Add files via upload
    Código que fiz na aula para um treino, onde verificar se um CPF está correto, seguindo o cálculo que deve ser feito, também foi configurado para poder inserir os números juntos, podendo até possuir carácteres especiais no meio(por exemplo: . , - e outros) sem problema algum.

---
+++ verificadorcpf.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool todosIguais(int cpf[])
+{
+    int i;
+    for(i=1; i<11;i++){
+        if(cpf[0] != cpf[i]) return false;
+    }
+    return true;
+}
+
+int calculaDigito(int cpf[], int mult[], int N){
+    int i;
+    int soma=0;
+    for(i=0;i<N;i++)
+    { soma+= cpf[i]*mult[i];
+    }
+    return ((soma*10)%11)%10;
+}
+
+int main()
+{
+    int cpf[11];
+    int m1[9]={10,9,8,7,6,5,4,3,2};
+    int m2[10]={11,10,9,8,7,6,5,4,3,2};
+    int i;
+    char entrada[20];
+    printf("Insira o seu CPF: ");
+    fgets(entrada, 20, stdin);
+    for(i=0;i<20;i++){
+        int j;
+        if(entrada[i]>=48 && entrada[i]<=57){ 
+            cpf[j]=entrada[i]-48;
+            j++;
+        }
+    }
+    if(!todosIguais(cpf) &&
+        calculaDigito(cpf, m1, 9) == cpf[9] && 
+        calculaDigito(cpf, m2, 10) == cpf[10]){
+        printf("\nCPF válido.");
+    } 
+    else printf("\nErro no CPF.");
+    return 0;
+}
